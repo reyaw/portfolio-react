e07d5776c24c8e19b1d031c92cff976635818c73
reyaw <reyaw@protonmail.com>
2021-08-24 18:22:29 -0400

    building out with material-ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import GitHubCards from './components/GitHubCards';
 import StickyFooter from './components/StickyFooter';
+import { ButtonGroup, Container } from '@material-ui/core';
 import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
 function App() {
   return (
-    <div>
+    <Container maxWidth="xl">
       <GitHubCards />
-      <Button href="#" variant="contained" color="secondary">
-        Hello World
-      </Button>
+      <ButtonGroup 
+      variant="contained" 
+      color="primary"
+      >
+        <Button
+          startIcon={<SaveIcon />}
+          size="large"
+          style={{
+            fontSize: "30px"
+          }}
+          href="#"
+          >
+          Save
+        </Button>
+        <Button
+          startIcon={<DeleteIcon />}
+          size="large"
+          style={{
+            fontSize: "30px"
+          }}
+          href="#"
+          variant="contained"
+          color="secondary">
+          Delete
+        </Button>
+      </ButtonGroup>
       <StickyFooter />
-    </div>
+    </Container>
   );
 }
 
